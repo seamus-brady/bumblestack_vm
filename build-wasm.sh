c12ba616c1946a28e33d4a548986eb6b72eef3d9
Seamus Brady <seamus@corvideon.ie>
2021-06-24 00:02:30 +0100

    Initial attempt at web demo design

--- build-wasm.sh
+++ build-wasm.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 EMSDK_ROOT=lib/emsdk
-SRC_ROOT=src/vm
-WASM_BUILD_ROOT=./wasm-build
+C_SRC_ROOT=src/vm
+WEB_SRC_ROOT=src/web
+WASM_BUILD_ROOT=./build/wasm
+DIST_ROOT=./build/dist
 
 # set emscripten env
 cd $EMSDK_ROOT
@@ -10,11 +12,17 @@ cd $EMSDK_ROOT
 cd ../..
 
 # create the wasm
-emcc $SRC_ROOT/*.c  -DWASM_ONLY_FUNCTIONALITY_ENABLED \
+emcc $C_SRC_ROOT/*.c  -DWASM_ONLY_FUNCTIONALITY_ENABLED \
 -s TOTAL_MEMORY=1024MB \
--o $WASM_BUILD_ROOT/bumblestack.html \
+-o $WASM_BUILD_ROOT/bumblevm.html \
 -s ERROR_ON_UNDEFINED_SYMBOLS=0 \
 -s EXPORTED_FUNCTIONS="['_main']" \
 -s EXPORTED_RUNTIME_METHODS=["cwrap"] \
--Oz --closure 1 \
 -v
+#-Oz --closure 1 \
+
+# copy the files
+cp -R $WEB_SRC_ROOT/assets $DIST_ROOT/assets
+cp -R $WEB_SRC_ROOT/index.html $DIST_ROOT
+cp $WASM_BUILD_ROOT/bumblevm.js $DIST_ROOT/assets
+cp $WASM_BUILD_ROOT/bumblevm.wasm $DIST_ROOT/assets
