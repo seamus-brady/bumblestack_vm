28a75222683461277d435cf4f2bebd2a1f6dd180
Seamus Brady <seamus@corvideon.ie>
2021-08-02 12:47:32 +0100

    Working python/c communication

--- src/bash/copy_lib_post_build.sh
+++ src/bash/copy_lib_post_build.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+DIR="$(cd "$(dirname "$0")" && pwd)"
+echo "Running from $DIR..."
+
 BUILD_DIR="./cmake-build-debug"
 LIB_NAME="libbumblestack.so"
 PY_DIR="./src/python/"
 
-echo "Hello!"
-cp $BUILD_DIR/$LIB_NAME PY_DIR
+echo "Changing directory..."
+cd ../..
+
+cp $BUILD_DIR/$LIB_NAME $PY_DIR
+echo "$LIB_NAME copied!"
